8c5453acc6f2a1139ead3acc49db34be72e3cbfb
Sidem <antoine.adjamidis@gmail.com>
2023-02-21 11:42:59 +0100

    forst commit

---
+++ components/Footer.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+export default function Footer() {
+
+    return (
+        <View>
+          Je suis le footer
+        </View>
+    )
+}
