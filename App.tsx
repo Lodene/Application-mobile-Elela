fb393c85c4abbed845153dd0d6c026b77838ffb0
Sidem <antoine.adjamidis@gmail.com>
2023-02-21 11:43:48 +0100

    init data

--- App.tsx
+++ App.tsx
@@ -6,7 +6,6 @@
  */
 
 import React from 'react';
-import Footer from './components/Footer'
 import type {PropsWithChildren} from 'react';
 import {
   SafeAreaView,
@@ -64,13 +63,36 @@ function App(): JSX.Element {
   };
 
   return (
-    <View>
-      <Footer />
-      <Text>
-      
-      </Text>
-    </View>
-    
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <Header />
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          }}>
+          <Section title="Step One">
+            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
+            screen and then come back to see your edits.
+          </Section>
+          <Section title="See Your Changes">
+            <ReloadInstructions />
+          </Section>
+          <Section title="Debug">
+            <DebugInstructions />
+          </Section>
+          <Section title="Learn More">
+            Read the docs to discover what to do next:
+          </Section>
+          <LearnMoreLinks />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
